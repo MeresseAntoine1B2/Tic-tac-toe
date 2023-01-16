869ac3ca90d96607019a6515ef01c55c2a1710f3
MeresseAntoine1B2 <antoine@meresse.info>
2023-01-16 10:30:01 +0100

    Ajout de la fonction est_coup_valide

--- tictactoe.py
+++ tictactoe.py
@@ -31,3 +31,28 @@ def jouer_coup(plateau:dict, joueur:str, coup:str) -> None:
     """
     plateau[coup[0]][int(coup[1])] = joueur
     
+def est_coup_valide(plateau:dict, coup:str) -> bool:
+    """Fonction qui vérifie si un coup est valide
+
+    Args:
+        plateau (dict): Le plateau de jeu
+        coup (str): Un coup de la forme "A1" avec le premier caractere entre A et C et le second entre 0 et 2 
+
+    Returns:
+        bool: True si le coup est valide, False sinon
+    """
+    
+    # On vérifie si le coup est une case qui existe
+    if coup[0].upper() not in ["A", "B", "C"]:
+        return False
+    if coup[1] not in ["0", "1", "2"]:
+        return False
+    
+    # On vérifie si la case est vide
+    if plateau[coup[0].upper()][int(coup[1])] == None:
+        return True
+    else:
+        return False
+    
+    
+    
